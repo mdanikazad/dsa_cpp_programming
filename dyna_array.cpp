f4141a3c9d7e5dc3317eef276aad6643409a68b5
Onik Azad <62051809+mdanikazad@users.noreply.github.com>
2023-12-21 22:48:16 +0800

    dyna_array.cpp

---
+++ dyna_array.cpp
@@ -0,0 +1,46 @@
+// Question: At first in the main function take an integer N as input. Then make a function named get_array() which will receive that N as a parameter. Then inside the get_array() function create an integer array of size N. Then the values of that array will be taken as input. After that return that array from that function and receive it in the main function and print the values of the array there.
+/*
+Sample Output
+5
+1 2 3 4 5
+
+Sample Output
+1 2 3 4 5
+
+Sample Input
+7
+5 1 2 4 6 9 3
+
+Sample Output
+5 1 2 4 6 9 3
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int* get_array(int n)
+{
+    
+    int * a = new int[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    
+     return a;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int * arr = get_array(n);
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] <<" ";
+    }
+
+    return 0;
+}
